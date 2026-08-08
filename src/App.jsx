23a4a4ef9971898ccf57d78ee631d0f94f61d5f5
newwmohan-jpg <newwmohan@gmail.com>
2026-08-09 00:25:36 +0530

    fetched photo and video data from unsplash and pexels API using axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 
+import { fetchPhotos , fetchVideo } from './APIdata/MediaApi'
+
+
 const App = () => {
   return (
     <div>
-      
+      <button 
+      className='bg-black text-white p-4 m-10 rounded-2xl'
+      onClick={ async ()=>
+        {const data = await fetchPhotos('dog')
+
+          console.log(data.results);
+        }
+      }>
+        images
+      </button>
+      <button 
+      className='bg-black text-white p-4 m-10 rounded-2xl'
+      onClick={ async ()=>
+        {const data = await fetchVideo('dog')
+
+          console.log(data.videos);
+        }
+      }>
+      videos
+      </button>
     </div>
   )
 }
